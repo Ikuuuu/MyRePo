233847c4542cd1c1ebd72dae0966170f037faefa
ikuuuu <songjx89@gmail.com>
2025-02-25 21:31:21 +0900

    day2 example add

---
+++ day2/arithmetic_operations.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+def power(a, b):
+    return a ** b
+
+def mod(a, b):
+    return a % b
+
+
+
+# Example usage
+if __name__ == "__main__":
+
+    x = 10
+    y = 5
+
+    print(f"{x} + {y} = ", add(x, y))
+    print(f"{x} - {y} = ", subtract(x, y))
+    print(f"{x} * {y} = ", multiply(x, y))
+    print(f"{x} / {y} = ", divide(x, y))
+    print(f"{x}, {y} power : ", power(x, y))
+    print(f"{x}, {y} mode : ", mod(x, y))
