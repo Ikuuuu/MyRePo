cb206e3d6f612e50855135685fde90b2e0748651
ikuuuu <songjx89@gmail.com>
2025-02-25 21:43:40 +0900

    code fix commit

--- day2/arithmetic_operations.py
+++ day2/arithmetic_operations.py
@@ -16,9 +16,9 @@ def power(a, b):
     return a ** b
 
 def mod(a, b):
+    if b == 0:
+        raise ValueError("Cannot mod by zero")
     return a % b
-
-
 
 # Example usage
 if __name__ == "__main__":
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     print(f"{x} - {y} = ", subtract(x, y))
     print(f"{x} * {y} = ", multiply(x, y))
     print(f"{x} / {y} = ", divide(x, y))
-    print(f"{x}, {y} power : ", power(x, y))
-    print(f"{x}, {y} mode : ", mod(x, y))
+    print(f"{x} ^ {y} = ", power(x, y))
+    print(f"{x} % {y} = ", mod(x, y))
